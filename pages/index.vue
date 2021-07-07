906424a369a478c866ef8a7aa509f2faa89cc4bb
AbisaiSan <abisaiweb7@gmail.com>
2021-07-07 14:14:13 -0300

    primeiro commit

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+ <div class="w-1/2 mx-auto my-20">
+
+  
+  <h1 class="text-3xl font-semibold text-red-600">{{tituloBlog}}</h1>
+
+   <article v-for=" verso in listaArtigos" class="my-20">
+
+     <h2>Informações do livro</h2>
+     <h4 class="text-5xl font-semibold mb-5">{{verso.name}}</h4>
+     <h4 class="text-5xl font-semibold mb-5">{{verso.author}}</h4>
+     <h4 class="text-5xl font-semibold mb-5">{{verso.group}}</h4>
+     <h4 class="text-5xl font-semibold mb-5">{{verso.version}}</h4>
+
+     <p class="mb-5">Capitulo: {{verso.chapter}} </p> 
+      <p class="mb-5">Texto:{{verso.verses}}</p>
+     <p class="text-red-500">Leia mais...</p>
+
+   </article>
+
+ </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      listaArtigos:[{}],
+      tituloBlog: 'Biblia Online'
+    }
+  },
+  created(){
+    this.fetch();
+  },
+  methods:{
+        async fetch() {
+          this.listaArtigos = await fetch(
+            'https://www.abibliadigital.com.br/api/verses/nvi/rm/8'
+            ).then(res => res.json())
+        }
+    }
+}
+
+</script>
