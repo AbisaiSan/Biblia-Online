3f332871e322e17727ba5e99eb0986196263f168
AbisaiSan <abisaiweb7@gmail.com>
2021-07-07 22:44:51 -0300

    Modificações api

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,47 @@
 <template>
- <div class="w-1/2 mx-auto my-20">
+  <div class="w-1/2 mx-auto my-20">
+    <h1 class="text-3xl font-semibold text-red-600">{{ tituloBlog }}</h1>
 
-  
-  <h1 class="text-3xl font-semibold text-red-600">{{tituloBlog}}</h1>
+    <article class="my-20">
+      <h2>Informações do livro</h2>
+      <h4 class="text-5xl font-semibold mb-5">
+        Livro: {{ listaArtigos.book }}
+      </h4>
+      <h4 class="text-5xl font-semibold mb-5">
+        Autor: {{ listaArtigos.author }}
+      </h4>
+      <h4 class="text-5xl font-semibold mb-5">
+        Gênero Literario: {{ listaArtigos.group }}
+      </h4>
+      <h4 class="text-5xl font-semibold mb-5">
+        Versão: {{ listaArtigos.version }}
+      </h4>
 
-   <article v-for=" verso in listaArtigos" class="my-20">
-
-     <h2>Informações do livro</h2>
-     <h4 class="text-5xl font-semibold mb-5">{{verso.name}}</h4>
-     <h4 class="text-5xl font-semibold mb-5">{{verso.author}}</h4>
-     <h4 class="text-5xl font-semibold mb-5">{{verso.group}}</h4>
-     <h4 class="text-5xl font-semibold mb-5">{{verso.version}}</h4>
-
-     <p class="mb-5">Capitulo: {{verso.chapter}} </p> 
-      <p class="mb-5">Texto:{{verso.verses}}</p>
-     <p class="text-red-500">Leia mais...</p>
-
-   </article>
-
- </div>
+      <p class="mb-5">Capitulo: {{ listaArtigos.chapter }}</p>
+      <p class="mb-5">Capitulo: {{ listaArtigos.chapter }}</p>
+      <p class="mb-5">Texto:{{ listaArtigos.verses }}</p>
+      <p class="text-red-500">Leia mais...</p>
+    </article>
+  </div>
 </template>
 
 <script>
-
 export default {
-  data(){
-    return{
-      listaArtigos:[{}],
-      tituloBlog: 'Biblia Online'
-    }
+  data() {
+    return {
+      listaArtigos: [],
+      tituloBlog: "Biblia Online",
+    };
   },
-  created(){
+  created() {
     this.fetch();
   },
-  methods:{
-        async fetch() {
-          this.listaArtigos = await fetch(
-            'https://www.abibliadigital.com.br/api/verses/nvi/rm/8'
-            ).then(res => res.json())
-        }
-    }
-}
-
+  methods: {
+    async fetch() {
+      this.listaArtigos = await fetch(
+        "https://www.abibliadigital.com.br/api/verses/nvi/rm/8"
+      ).then((res) => res.json());
+    },
+  },
+};
 </script>
